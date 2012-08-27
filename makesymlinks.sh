591e5ca7b055842377625a70a0d2b7a102b2fc50
“Gregory <gjorgensen@visualpoint.com>
2012-08-27 17:42:01 -0400

    bug fix

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -29,5 +29,5 @@ for file in $files; do
 echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file $olddir/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/.$file ~/.$file
 done
